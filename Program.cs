fcc84c83fcf579e72cdff66932d5630e15f7d6b8
AmirabbasDorasay <46056699+AmirabbasDorasay@users.noreply.github.com>
2021-02-21 21:40:49 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class BinaryConverter
+{
+    public static void Main()
+    {
+        //variables section
+        string binary;
+
+        string result;
+
+        //input section
+        Console.Write("Yek Adad Vared Konid : ");
+        binary = Console.ReadLine();
+
+        //convert and result section
+        int num = Convert.ToInt32(binary);
+        result = "";
+        while (num > 1)
+        {
+            int reminder = num % 2;
+            result = Convert.ToString(reminder) + result;
+            num /= 2;
+        }
+        result = Convert.ToString(num) + result;
+        Console.WriteLine("Converted To Binary: {0}", result);
+
+
+        Console.ReadKey();
+        //developed by : Amirabbas Dorasay
+    }
+}
